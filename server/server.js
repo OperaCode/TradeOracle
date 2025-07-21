0013a466aef8959fa728a79b50bac04a7454b73b
Raphael Faboyinde <raphaelfaboyinde27@gmail.com>
2025-07-21 20:31:07 +0100

    cors

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 // CORS config 
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://trade-oracle.vercel.app/"], 
+    origin: ["http://localhost:5173", "https://trade-oracle.vercel.app"], 
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true, 
   })
